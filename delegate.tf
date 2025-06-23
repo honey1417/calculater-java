8a281f9dbf7917f388b04daea44682ed8c7c322c
harshini <harshinipamarthi@gmail.com>
2025-06-23 13:54:06 +0530

    c3

--- delegate.tf
+++ delegate.tf
@@ -26,3 +26,18 @@ provider "helm" {
   }
 }
 
+module "delegate" {
+  source = "harness/harness-delegate/kubernetes"
+  version = "0.2.2"
+  account_id = "ucHySz2jQKKWQweZdXyCog"
+  delegate_token = "NTRhYTY0Mjg3NThkNjBiNjMzNzhjOGQyNjEwOTQyZjY="
+  delegate_name = "terraform-delegate-harshini"
+  deploy_mode = "KUBERNETES"
+  namespace = "harness-delegate-ng"
+  manager_endpoint = "https://app.harness.io"
+  delegate_image = "us-docker.pkg.dev/gar-prod-setup/harness-public/harness/delegate:25.06.86100"
+  replicas = 1
+  upgrader_enabled = true
+  
+  depends_on = [google_container_cluster.primary]
+}
