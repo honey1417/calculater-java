81db847d6439f38b6dbcb0cc59091ac806ba65a1
harshini <harshinipamarthi@gmail.com>
2025-06-23 14:39:34 +0530

    c8

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.0.0"
+    }
+  }
+}
+
 provider "google" {
     project = var.project_id
     region = var.region
